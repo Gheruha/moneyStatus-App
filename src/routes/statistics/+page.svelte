6d3ed1516bd611a0812c79c5f81cd7355b4e5ce4
Gheruha <gheruham@icloud.com>
2024-04-25 19:21:49 +0300

    ✨ Animations for statistics page

--- src/routes/statistics/+page.svelte
+++ src/routes/statistics/+page.svelte
@@ -14,6 +14,37 @@
 		month: 'long',
 		year: 'numeric'
 	});
+	let width_income = 0;
+	let width_expense = 0;
+	let income_percent = 0;
+	let expense_percent = 0;
+
+	// functions
+	function getPercent(income_perc, expense_perc) {
+		income_percent = Math.floor(income_perc); // Extract integer part
+		expense_percent = Math.floor(expense_perc);
+	}
+	function increaseWidthIncome() {
+		if (income_percent != null) {
+			if (width_income != income_percent) {
+				width_income++;
+			} else if (width_income == income_percent) {
+				clearInterval(intervalIdIncome);
+			}
+		}
+	}
+	let intervalIdIncome = setInterval(increaseWidthIncome, 5);
+
+	function increaseWidthExpense() {
+		if (expense_percent != null) {
+			if (width_expense != expense_percent) {
+				width_expense++;
+			} else if (width_expense == expense_percent) {
+				clearInterval(intervalIdExpense);
+			}
+		}
+	}
+	let intervalIdExpense = setInterval(increaseWidthExpense, 5);
 
 	// Add date in the database
 	async function addDate(date) {
@@ -69,6 +100,9 @@
 				>
 			</div>
 			{#each data.data as percentages}
+				<div class="h-0 opacity-0">
+					{getPercent(percentages.income_percent, percentages.expense_percent)}
+				</div>
 				<p class="text-blue-500">
 					Income %: {percentages.income_percent !== null
 						? percentages.income_percent.toFixed(2)
@@ -76,9 +110,7 @@
 				</p>
 				<div
 					class="bg-blue-300 rounded-lg"
-					style="width: {percentages.income_percent !== null
-						? percentages.income_percent + '%'
-						: '0%'}"
+					style="width: {width_income !== null ? width_income + '%' : '0%'}"
 				>
 					&nbsp;
 				</div>
@@ -89,9 +121,7 @@
 				</p>
 				<div
 					class="bg-red-300 rounded-lg"
-					style="width: {percentages.expense_percent !== null
-						? percentages.expense_percent + '%'
-						: '0%'}"
+					style="width: {width_expense !== null ? width_expense + '%' : '0%'}"
 				>
 					&nbsp;
 				</div>
