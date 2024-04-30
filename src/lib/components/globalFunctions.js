a1abc530ad7050d416ef1855820f5608bb4442b4
Gheruha <gheruham@icloud.com>
2024-04-30 22:29:09 +0300

    ✅ JS file for global functions

---
+++ src/lib/components/globalFunctions.js
@@ -0,0 +1,46 @@
+export function formatDay(dateString) {
+	const date = new Date(dateString);
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+
+	return `${year}-${month}-${day}`;
+}
+
+async function addDate(date) {
+	const data = JSON.stringify(date);
+	await fetch('api/setMonth', {
+		method: 'POST',
+		body: data,
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+	location.reload();
+}
+export function increaseMonth(date) {
+	const dateObj = new Date(date);
+
+	dateObj.setMonth(dateObj.getMonth() + 1);
+	date = formatDay(dateObj);
+	addDate(date);
+}
+
+export function decreaseMonth(date) {
+	const dateObj = new Date(date);
+	dateObj.setMonth(dateObj.getMonth() - 1);
+	date = formatDay(dateObj);
+	addDate(date);
+}
+
+export async function deleteData(money_id) {
+	const data = JSON.stringify(money_id);
+	await fetch('api/deleteData', {
+		method: 'POST',
+		body: data,
+		headers: {
+			'content-type': 'application/json'
+		}
+	});
+	location.reload();
+}
